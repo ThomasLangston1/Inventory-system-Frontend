a96f5f3c1d5c95d6063c95ab27c00376d4bf5ea1
ThomasLangston1 <Thomaslangston10@gmail.com>
2019-12-18 17:09:36 -0600

    Test

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <v-app>
     <v-app-bar app color="primary" dark>
+      <v-btn active-class="/inventory" color="primary">Primary</v-btn>
       <v-spacer></v-spacer>
       <v-btn to="/login" text color="black">Login/Create Account</v-btn>
     </v-app-bar>
